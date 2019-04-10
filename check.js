651515139fbef183af8b97bc913d91aac3fd9495
tameem6 <tameem6ansar@gmail.com>
2019-04-10 17:02:32 +0530

    2nd Assignment

--- check.js
+++ check.js
@@ -1,69 +1,63 @@
-function calcEve()
+function cubeCalc()
 {
-  var x=document.getElementById("ooe").value;
-  x=parseInt(x);
-  if(x%2===0)
+    var a=document.getElementById("cub").value;
+    a=a.split(" ").map(Number);
+    var cube=[];
+    let i=0,j=0;
+    for(i=0;i<a.length;i++)
     {
-    alert("Even");
+        if(a[i]%2==0)
+        {
+            cube[j++]=Math.pow(a[i],3);
+        }
     }
-  else
-    alert("Odd");
+    alert(cube);
 }
-function calcFib()
+function sdCalc()
 {
-    var a=document.getElementById("fib").value;
-    let y1=0,y2=1;
-    var res,i,msg="";
-    if(a<=0)
+    var ar=document.getElementById("stan").value;
+    ar=ar.split(" ").map(Number);
+    for(var i=0;i<ar.length;i++)
     {
-        alert("Invalid Input");
+        ar[i]=parseInt(ar[i]);
     }
-    else if(a==1)
+    var sum = ar.reduce(function(a, b) { return a + b; }, 0);
+    var mean=sum/ar.length;
+    var dif=[];
+    dif=ar.map(function(a){ return a-mean;},0);
+    var sd;
+    sd=dif.reduce(function(a,b){return Math.pow(a,2)+Math.pow(b,2);});
+    sd=Math.sqrt(sd)/dif.length;
+    alert(sd);
+}
+function sumAll(... args)// Call in arCheck)()
+{
+    let sum=0;
+    for(let arg of args)
     {
-        alert("0");
+        sum+=arg;
     }
+    return sum;
+}
+
+function argCheck()
+{
+    let arr = document.getElementById("args").value;
+    arr=arr.split(" ").map(Number);
+    let s=sumAll(... arr);
+    alert(s);
+}
+function flip()
+{
+    let c=Math.floor(Math.random()*2);
+    if(c==0)
+        alert("Heads!");
     else
-    {
-        msg=y1+" "+y2;
-        for(i=2;i<=a;i++)
-        {
-            res=y1+y2;
-            msg+=" "+res;
-            y1=y2;
-            y2=res;
-        }
-    alert(msg);
-    }
+        alert("Tails!");
 }
-function calcPrime()
+function hide()
 {
-    var p=document.getElementById("prim").value;
-    var i,t=0;
-    for(i=2;i<p;i++)
-    {
-        if(p%i==0)
-        {
-            alert("Not a prime");
-            t=-1;
-            break;
-        }
-    }
-    if(t==0)
-    {
-        alert("Prime");
-    }
-}
-function calcu()
-{
-    var v1=document.getElementById("var1").value;
-    var v2=document.getElementById("var2").value;
-    v1=parseInt(v1);
-    v2=parseInt(v2);
-    var s,d,q,pr,r;
-    s=v1+v2;
-    d=v1-v2;
-    q=v1/v2;
-    pr=v1*v2;
-    r=v1%v2;
-    alert(s+" "+d+" "+q+" "+pr+" "+r);
+    var str = document.getElementById("cli").value;
+  var arr = str.split(" ");
+  alert(arr);
 }
